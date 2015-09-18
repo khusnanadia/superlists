d21084bd042ef2225b3270a30d6b8af8484ec972
khusnanadia <khusnanadia@gmail.com>
2015-09-18 23:50:29 +0000

    -

--- functional_tests.py
+++ functional_tests.py
@@ -5,12 +5,12 @@ import unittest
 class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
-#		self.browser.implicitly_wait(3)
+		self.browser.implicitly_wait(3)
 
 	def tearDown(self):
 		self.browser.quit()
 
-	def check_for_row_in_list_table(self):
+	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text, [row.text for row in rows])
@@ -49,14 +49,24 @@ class NewVisitorTest(unittest.TestCase):
 
 		# There is still a text box inviting her to add another item.
 		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		self.fail('Finish the test!')
-		
+		inputbox = self.browser.find.element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+
 		# The page updates again, and now shows both items on her list
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find.elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+		self.assertIn(
+			'2: Use peacock feathers to make a fly',
+			[row.text for row in rows]
+		)
 
 		# Edith wonders whether the site will remember her list. Then she sees
 		# that the site has generated a unique URL for her -- there is some
 		# explanatory text to that effect
-
+		self.fail('Finish the test!')
+		
 		# She visits that URL - her to-do list is still there.
 
 		# Satisfied, she goes back to sleep

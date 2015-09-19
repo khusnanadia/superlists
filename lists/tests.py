f39a3c634c50a4dd7c6004b10a6632974bc9e7ff
khusnanadia <khusnanadia@gmail.com>
2015-09-19 00:20:28 +0000

    Model for list Items and associated migration

--- lists/tests.py
+++ lists/tests.py
@@ -2,7 +2,7 @@ from django.template.loader import render_to_string
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
-
+from lists.models import Item
 from lists.views import home_page
 
 # Create your tests here.
@@ -18,9 +18,9 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
-#		self.assertTrue(response.content.startswith(b'<html>'))
-#		self.assertIn(b'<title>To-Do lists</title>', response.content)
-#		self.assertTrue(response.content.strip().endswith(b'</html>'))
+		self.assertTrue(response.content.startswith(b'<html>'))
+		self.assertIn(b'<title>To-Do lists</title>', response.content)
+		self.assertTrue(response.content.strip().endswith(b'</html>'))
 
 	def test_home_page_can_save_a_POST_request(self):
 		request = HttpRequest()
@@ -28,10 +28,50 @@ class HomePageTest(TestCase):
 		request.POST['item_text'] = 'A new list item'
 
 		response = home_page(request)
+		
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
 
-		self.assertIn('A new list item', response.content.decode())
-		expected_html = render_to_string(
-			'home.html',
-			{'new_item_text': 'A new list item'}
-		)
-		self.assertEqual(response.content.decode(), expected_html)
+	def test_home_page_redirect_after_POST(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+
+		response = home_page(request)
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+
+	def test_home_page_only_saves_item_when_necessary(self):
+		request = HttpRequest()
+		response = home_page(request)
+		self.assertEqual(Item.objects.count(), 0)
+
+	def test_home_page_displays_all_list_items(self):
+		Item.objects.create(text='itemey 1')
+		Item.objects.create(text='itemey 2')
+
+		request = HttpRequest()
+		response = home_page(request)
+
+		self.assertIn('itemey 1', response.content.decode())
+		self.assertIn('itemey 2', response.content.decode())
+
+class ItemModelTest(TestCase):
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item.text = 'The first (ever) list item'
+		first_item.save()
+
+		second_item = Item()
+		second_item.text = 'Item the second'
+		second_item.save()
+
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(second_saved_item.text, 'Item the second')

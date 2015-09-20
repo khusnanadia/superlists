502ebdebaed0159e87641b06eb0f3c94449476ce
Khusna Nadia <khusnanadia@gmail.com>
2015-09-20 15:01:10 +0700

    nyamain sama buku

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,16 +1,15 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from django.test import LiveServerTestCase
 #import unittest
 
 class NewVisitorTest(LiveServerTestCase):
-
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
 
-#	def tearDown(self):
-#		self.browser.quit()
+	def tearDown(self):
+		self.browser.quit()
 
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')

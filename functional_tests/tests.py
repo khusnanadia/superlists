7a953e983653fb91c8f0766fc606987ee06a25f8
Khusna Nadia <khusnanadia@gmail.com>
2015-09-20 17:52:51 +0700

    ngopy dari buku

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,20 +1,22 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-	
+
 class NewVisitorTest(LiveServerTestCase):
 
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
-
+	
 	def tearDown(self):
 		self.browser.quit()
+
 
 	def check_for_row_in_list_table(self, row_text):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
 		self.assertIn(row_text, [row.text for row in rows])
+
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
@@ -87,3 +89,4 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn('Buy milk', page_text)
 
 		# Satisfied, they both go back to sleep
+
--- superlists/urls.py
+++ superlists/urls.py
@@ -3,10 +3,6 @@ from lists import views as list_views
 from lists import urls as list_urls
 
 urlpatterns = [
-	# Examples:
-	# url(r'^$', 'superlists.views.home', name='home'),
-	# url(r'^blog/', include('blog.urls')),
-	
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
     # url(r'^admin/', include(admin.site.urls)),

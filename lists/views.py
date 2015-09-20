d43220097fe50a28387d44f56855f8c06996ea0c
Khusna Nadia <khusnanadia@gmail.com>
2015-09-20 18:23:36 +0700

    ganti jadi tab

--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import redirect, render
 from lists.models import Item, List
 #test
 def home_page(request):
-    comment = ''
+	comment = ''
 	if Item.objects.count() == 0:
 		comment = 'yey, waktunya berlibur'
 	elif Item.objects.count() < 5:
@@ -12,20 +12,20 @@ def home_page(request):
 
 	return render(request, 'home.html', {'comment':comment})
 
-
+	
 def new_list(request):
-    list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
+	list_ = List.objects.create()
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect('/lists/%d/' % (list_.id,))
 
 
 def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
+	list_ = List.objects.get(id=list_id)
+	Item.objects.create(text=request.POST['item_text'], list=list_)
+	return redirect('/lists/%d/' % (list_.id,))
 
 
 def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_})
+	list_ = List.objects.get(id=list_id)
+	return render(request, 'list.html', {'list': list_})
 

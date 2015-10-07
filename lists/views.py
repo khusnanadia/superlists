ed4c269c850d2c646df34f4552868a9e5e5f4cc6
khusnanadia <khusnanadia@gmail.com>
2015-10-07 07:50:33 +0000

    bikin database baru

--- lists/views.py
+++ lists/views.py
@@ -2,14 +2,7 @@ from django.shortcuts import redirect, render
 from lists.models import Item, List
 #test
 def home_page(request):
-	comment = ''
-#	if Item.objects.count() == 0:
 	comment = 'yey, waktunya berlibur'
-#	elif Item.objects.count() < 5:
-#		comment = 'sibuk tapi santai'
-#	else:
-#		comment = 'oh tidak'
-
 	return render(request, 'home.html', {'comment':comment})
 
 	
@@ -30,13 +23,10 @@ def view_list(request, list_id):
 	itemList = Item.objects.filter(list_id=list_id)
 
 	comment = ''
-#	if Item.objects.filter(list_id=list_id).count() == 0:
 	if itemList.count() == 0:
 		comment = 'yey, waktunya berlibur'
-#	elif Item.objects.filter(list_id=list_id).count() < 5:
 	elif itemList.count() < 5:
 		comment = 'sibuk tapi santai'
-#	elif Item.objects.filter(list_id=list_id).count() >= 5:
 	else:
 		comment = 'oh tidak'
 
